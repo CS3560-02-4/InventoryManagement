d244e6bd8f1a5d5537de8d00335eb41cc12e5fa5
sherelene <sherelenedebelen@gmail.com>
2022-04-19 10:56:21 -0700

    connected mysql jar file to IDE

--- InventorySystem/src/database.java
+++ InventorySystem/src/database.java
@@ -1,3 +1,41 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.*;
+
 public class database {
-    
+
+    public static void main(String[] args){
+    String url = "jdbc:mysql://127.0.0.1:3306/?user=root";
+    String user = "root";
+    String password = "12669Rancho.";
+    Connection myConn;
+
+    {
+        try {
+            myConn = DriverManager.getConnection(url,user,password);
+            Statement myStmt = myConn.createStatement();
+            String sql = "select * from our_company.product_1";
+            ResultSet rs = myStmt.executeQuery(sql);
+
+            if (myConn != null) {
+                System.out
+                        .println("Successfully connected to MySQL database test");
+            }
+
+            while (rs.next())
+
+                System.out.println(rs.getString( "product_name"));
+
+
+        } catch (SQLException ex) {
+            System.out.println("An error occurred while connecting MySQL databse");
+                            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+    }
+
+
+}
 }
