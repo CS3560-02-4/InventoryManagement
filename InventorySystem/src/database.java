f97d20e015bfe5cdc22778d68f915d11a9111742
sherelene <sherelenedebelen@gmail.com>
2022-04-19 20:48:12 -0700

    example

--- InventorySystem/src/database.java
+++ InventorySystem/src/database.java
@@ -5,7 +5,7 @@ import java.sql.Statement;
 import java.sql.*;
 
 
-/* set the CLASSPATH to the current folder and
+/*
 install the .jar file at
 C:\Program Files\MySQL\MySQL Connector J\mysql-connector-java-5.1.32-bin.jar.
   by going to File -> Program structure -> modules -> dependencies -> (press + button) ->
