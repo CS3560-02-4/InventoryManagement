1b85c284170ca19670eec14eac7a4c9686e338ae
jammer1510 <timmylin0831@gmail.com>
2022-04-20 19:50:01 -0700

    database

--- InventorySystem/src/database.java
+++ InventorySystem/src/database.java
@@ -10,24 +10,24 @@ install the .jar file at
 C:\Program Files\MySQL\MySQL Connector J\mysql-connector-java-5.1.32-bin.jar.
   by going to File -> Program structure -> modules -> dependencies -> (press + button) ->
   (add jar file from file location)*/
-
+/* wjdaklsdjao */
 public class database {
 
     public static void main(String[] args){
     String url = "jdbc:mysql://127.0.0.1:3306/?user=root";
     String user = "root";
-    String password = "12669Rancho.";
+    String password = "timmy1431";
     Connection myConn;
 
     {
         try {
             myConn = DriverManager.getConnection(url,user,password);
             Statement myStmt = myConn.createStatement();
-            String sql = "select * from our_company.product_1";
+            String sql = "select * from our_company.product";
             ResultSet rs = myStmt.executeQuery(sql);
 
             if (myConn != null) {
-                System.out.println("Successfully connected to MySQL database test yayyy");
+                System.out.println("Successfully connected to MySQL database test");
             }
 
             while (rs.next())
